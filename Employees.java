842e54e10012c464d37ad3f6514d02109b64e6ca
Sydney Trang <37758639+SydneyTrang@users.noreply.github.com>
2019-03-02 10:24:11 -0500

    Add files via upload

---
+++ Employees.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Sydney
+ */
+public class Employees {
+    String sIDNumber, sFullName, sAnnualSalary, sStartDate;
+
+    Employees(String sIDNum, String sName, String sSalary, String sSDate) {
+        sIDNumber = sIDNum;
+        sFullName = sName;
+        sAnnualSalary = sSalary;
+        sStartDate = sSDate;
+    }
+
+}
